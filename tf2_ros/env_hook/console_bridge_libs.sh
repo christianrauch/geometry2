c545575148f2581ea7513db47706baa2d1ca04a4
Christian Rauch <Rauch.Christian@gmx.de>
2017-10-12 23:05:43 +0000

    correctly determine the CPU architecture

--- tf2_ros/env_hook/console_bridge_libs.sh
+++ tf2_ros/env_hook/console_bridge_libs.sh
@@ -2,5 +2,5 @@
 
 # This is necessary due to console bridge installing to a non-standard location
 # https://github.com/ros2/ros2/issues/195
-PROCESSOR=`uname -p`
+PROCESSOR=`uname -m`
 ament_prepend_unique_value LD_LIBRARY_PATH "$AMENT_CURRENT_PREFIX/lib/$PROCESSOR-linux-gnu"
